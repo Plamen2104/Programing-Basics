b0e97c23fbc04e94e2e72beabc451aee5a127695
Plamen2104 <pertovplamen0@gmail.com>
2025-10-08 21:22:01 +0300

    Update ex7.cpp

--- ex7.cpp
+++ ex7.cpp
@@ -4,8 +4,11 @@
 
 int main()
 {
-    double x = scanf_s("%lf", &x);
-    double a = scanf_s("%lf", &a);
+    double x ;
+    scanf_s("%lf", &x);
+    double a ;
+    scanf_s("%lf", &a);
     double y = ((pow(x + a, 5)) + (abs(x + 2 * a)) / (5 * a + sqrt(pow(a, 5))));
     printf_s("%lf", y);
 }
+
