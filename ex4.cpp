98af389db631f3f69c5ef43be5706acb32ad33d6
Plamen2104 <pertovplamen0@gmail.com>
2025-10-08 21:21:12 +0300

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -4,8 +4,11 @@
 
 int main()
 {
-    double a = scanf_s("%lf", &a);
-    double b = scanf_s("%lf", &b);
+    double a ;
+    scanf_s("%lf", &a);
+    double b ;
+    scanf_s("%lf", &b);
     double y = ((pow(a,2)+(2*a*b)+pow(b,2))-(4*a*b) / sqrt(3*abs(a)));
     printf("%lf", y);
 }
+
