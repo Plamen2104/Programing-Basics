f2f9096bf871d165bfec48aaef2b24ff8de025ce
Plamen2104 <pertovplamen0@gmail.com>
2025-10-08 21:20:50 +0300

    ex5.cpp

--- ex5.cpp
+++ ex5.cpp
@@ -4,8 +4,11 @@
 
 int main()
 {
-    double x = scanf_s("%lf", &x);
-    double y = (abs(sqrt(2 + pow(x, 5) / pow(x,4) + 5*x)));
+    double x ;
+    scanf_s("%lf", &x);
+    double y ;
+    (abs(sqrt(2 + pow(x, 5) / pow(x,4) + 5*x)));
     printf_s("%lf", y);
 }
+
 
