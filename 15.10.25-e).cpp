acea49497c05cf662302086bd98b254227bfeabc
Plamen2104 <pertovplamen0@gmail.com>
2025-10-15 11:41:00 +0300

    Add files via upload

---
+++ 15.10.25-e).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a, b, x;
+    scanf_s("%lf", &x);
+    scanf_s("%lf", &a);
+    scanf_s("%lf", &b);
+    double y = (((5 * a * b) + (3 * pow(x, 5))) / sqrt((pow(a,2)+(2*a*b)-pow(b,2)) + 1 / 2)) * ((8 * x) / pow(b, 5));
+    printf_s("%.2lf", y);
+
+}
+
