f15036217728a6122213e7d0f549f70bc54c0d96
Plamen2104 <pertovplamen0@gmail.com>
2025-11-26 11:35:02 +0200

    Add files via upload

---
+++ 4etni i ne4etni.cpp
@@ -0,0 +1,19 @@
+﻿// 4etni i ne4etni.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+const int size = 10;
+#include <iostream>
+
+int main()
+{
+    float mA[size];
+    float value;
+    for (int i = 0; i < size;i++) {
+        scanf_s("%f", &value);
+        mA[i] = value;
+
+    }
+    for (int i = 0;i < size;i++) {
+        printf("%d\n", i);
+    }
+
+}
