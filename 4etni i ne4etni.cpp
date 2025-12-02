0ded26d660556ad9ba5f717d18543effa4f87661
Plamen2104 <pertovplamen0@gmail.com>
2025-12-02 17:58:55 +0200

    4etni i ne4etni.cpp

--- 4etni i ne4etni.cpp
+++ 4etni i ne4etni.cpp
@@ -1,19 +1,31 @@
-﻿// 4etni i ne4etni.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-const int size = 10;
-#include <iostream>
+#include <cstdio>
+#include <cmath>
 
-int main()
-{
-    float mA[size];
-    float value;
-    for (int i = 0; i < size;i++) {
-        scanf_s("%f", &value);
-        mA[i] = value;
+int main() {
+    double arr[10];
 
-    }
-    for (int i = 0;i < size;i++) {
-        printf("%d\n", i);
+   
+    printf("Enter 10 real numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf_s("%lf", &arr[i]);
     }
 
+   
+    for (int i = 0; i < 10; i++) {
+        
+        if (fmod(arr[i], 2) == 0) {
+            arr[i] = pow(arr[i], 3);   
+        }
+        else {
+            arr[i] += 10;             
+        }
+    }
+
+   
+    printf("The result is:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%.2f ", arr[i]);
+    }
+
+    return 0;
 }
