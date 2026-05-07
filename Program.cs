fe572cd92d0ec7f07b3e29becd1fd67e4d82bedf
Plamen2104 <pertovplamen0@gmail.com>
2026-05-07 09:04:59 +0300

    Add files via upload
    lab 7 oop

--- Program.cs
+++ Program.cs
@@ -1,45 +1,34 @@
-﻿namespace ConsoleApp2
+﻿namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+        }
 
+        class Rectangle {
 
+            private double heigh, width,area;
+
+            public Rectangle(double h, double w)
+            {
+                heigh = h;
+                width = w;
+            }
+            public void input()
+            {
+                Console.WriteLine("Enter Heigh: ");
+                heigh = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Weight: ");
+                width = double.Parse(Console.ReadLine());
+            }
+            public double Area 
+            {
+                get { return area; }
+                set { area = heigh * width; }
+            }
+        
         }
     }
-    class Point
-    {
-        private double x, y;
-        public void input()
-        {
-            Console.Write("x = ");
-            x = Double.Parse(Console.ReadLine());
-            Console.WriteLine("y = ");
-            y = Double.Parse(Console.ReadLine());
-        }
-        public void output()
-        {
-            Console.WriteLine("x: {0}   y: {1}", x, y);
-        }
-        public void Translation(double Vx,double Vy) {
-            x += Vx;
-            y += Vy;
-        }
-        public void Rotarion(double alphaDegrees)
-        {
-            double a = alphaDegrees * (Math.PI / 180.0);
-            double x1 = x* Math.Cos(a)  - y*Math.Sin(a);
-            double y1 = x*Math.Sin(a) + y*Math.Cos(a);
-
-            x = x1;
-            y = y1;
-        }
-        public void Scale(double kx, double ky)
-        {
-            x*= kx;
-            y*= ky;
-        }
-}    }    
-
-
+}
